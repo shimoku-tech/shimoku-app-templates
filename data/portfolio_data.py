d35f43f0222fffde2533ca00e24d3d88c4c9e472
capitan-ariete <sergi.ortiz.rodriguez@gmail.com>
2022-07-15 08:59:31 +0200

    First version ready

--- data/portfolio_data.py
+++ data/portfolio_data.py
@@ -1,93 +1,37 @@
 """
 """
 
-from os import getenv
-
-import datetime as dt
-import pandas as pd
-
-import shimoku_api_python as shimoku
 from shimoku_components_catalog.html_components import (
     create_h1_title, button_click_to_new_tab, beautiful_indicator,
     box_with_button, panel,
 )
 
-api_key: str = getenv('API_TOKEN')
-universe_id: str = getenv('UNIVERSE_ID')
-business_id: str = getenv('BUSINESS_ID')
-environment: str = getenv('ENVIRONMENT')
 
+data_html_real_time = create_h1_title(title='Real-time', subtitle='Users')
 
-s = shimoku.Client(
-    config={'access_token': api_key},
-    universe_id=universe_id,
-    environment=environment,
-)
-s.plt.set_business(business_id=business_id)
-menu_path: str = 'Portfolio'
-create_rt_indicators: bool = True
+data_indicator_real_time_events = [
+    {
+        'description': '27 / 05 / 2022 · 11:14:54',
+        'title': 'Events',
+        'value': '552,298',
+        'color': '',
+        'align': 'left'
+    }
+]
 
+data_indicator_real_time_time_session = [
+    {
+        'description': 'In minutes',
+        'title': 'Time Sessions AVG',
+        'value': '1,59',
+        'color': '',
+        'align': 'left'
+    }
+]
 
-html = create_h1_title(title='Real-time', subtitle='Users')
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=0, rows_size=1, cols_size=12,
-)
+data_html_summary = create_h1_title(title='Summary', subtitle='Results 28 days previous')
 
-
-if create_rt_indicators:
-    data_ = [
-        {
-            'description': '27 / 05 / 2022 · 11:14:54',
-            'title': 'Events',
-            'value': '552,298',
-            'color': '',
-            'align': 'left'
-        }
-    ]
-    s.plt.indicator(
-        data=data_,
-        menu_path=menu_path,
-        order=1, rows_size=1, cols_size=6,
-        value='value',
-        color='color',
-        header='title',
-        footer='description',
-        align='align'
-    )
-
-
-    data_ = [
-        {
-            'description': 'In minutes',
-            'title': 'Time Sessions AVG',
-            'value': '1,59',
-            'color': '',
-            'align': 'left'
-        }
-    ]
-    s.plt.indicator(
-        data=data_,
-        menu_path=menu_path,
-        order=2, rows_size=1, cols_size=6,
-        value='value',
-        color='color',
-        header='title',
-        footer='description',
-        align='align'
-    )
-
-
-html = create_h1_title(title='Summary', subtitle='Results 28 days previous')
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=3, rows_size=1, cols_size=12,
-)
-
-
-data = [
+data_line_sessions_date = [
     {'date': 1, 'Sessions': 360},
     {'date': 2, 'Sessions': 164},
     {'date': 3, 'Sessions': 132},
@@ -115,75 +59,34 @@ data = [
     {'date': 24, 'Sessions': 479},
     {'date': 25, 'Sessions': 560},
 ]
-s.plt.line(
-    data=data,
-    x='date', y=['Sessions'],
-    menu_path=menu_path,
-    order=4, rows_size=2, cols_size=7,
-    title='Total Sessions x day',
-    option_modifications={'dataZoom': False}
-)
 
-data_ = [
+data_pie = [
     {'name': 'Mobile', 'value': 12478},
     {'name': 'Tablet', 'value': 3217},
     {'name': 'Desktop', 'value': 9418},
 ]
 
-s.plt.pie(
-    data=data_,
-    x='name', y='value',
-    menu_path=menu_path,
-    order=5, rows_size=2, cols_size=5,
-    title='Devices',
-)
-
-
-html = beautiful_indicator(
+data_html_beautiful_indicator_anomaly_suite = beautiful_indicator(
     title='View Anomaly Suite',
     background_url='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a07a6d9e984908a5aca6a1_shim-anomaly-bg-s.jpg',
     href='https://develop.shimoku.io/big-bang',
 )
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=6, rows_size=1, cols_size=4,
-)
 
-
-html = beautiful_indicator(
+data_html_beautiful_indicator_retention_suite = beautiful_indicator(
     title='View Retention Suite',
     background_url='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a07a6dca821c951f9554e4_shim-retention-bg-s.jpg',
     href='https://develop.shimoku.io/big-bang',
 )
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=7, rows_size=1, cols_size=4,
-)
 
-
-html = beautiful_indicator(
+data_html_beautiful_indicator_stock_suite = beautiful_indicator(
     title='View Stock Suite',
     background_url='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a07a6d1dcb0d6fbb50159e_shim-stock-bg-s.jpg',
     href='https://develop.shimoku.io/big-bang',
 )
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=8, rows_size=1, cols_size=4,
-)
 
+data_html_title_cohort = create_h1_title(title='Cohort', subtitle='Results')
 
-html = create_h1_title(title='Cohort', subtitle='Results')
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=9, rows_size=1, cols_size=12,
-)
-
-
-data_ = [
+data_heatmap = [
         {
             "xAxis": "Monday",
             "yAxis": "12 a.m",
@@ -265,107 +168,42 @@ data_ = [
             "value": 6
         }
     ]
-s.plt.heatmap(
-        data=data_,
-        x='xAxis', y='yAxis',
-        value='value',
-        menu_path=menu_path,
-        order=10, rows_size=2, cols_size=12,
-)
 
+data_html_creativity = create_h1_title(title='Creativity analysis', subtitle='New campaign 2022')
 
-html = create_h1_title(title='Creativity analysis', subtitle='New campaign 2022')
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=11, rows_size=1, cols_size=12,
-)
-
-
-html = button_click_to_new_tab(
+data_html_pug = button_click_to_new_tab(
     title='Pug',
     background_url='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a080dbb92e78ae2b2cdbe2_pug.jpg',
     href='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a080dbb92e78ae2b2cdbe2_pug.jpg'
 )
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=12, rows_size=1, cols_size=3,
-)
 
-
-html = button_click_to_new_tab(
+data_html_chupa_chups = button_click_to_new_tab(
     title='Chupa-Chups',
     background_url='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a080db5f002762a688eae7_chup.jpg',
     href='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a080db5f002762a688eae7_chup.jpg',
 )
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=13, rows_size=1, cols_size=3,
-)
 
-
-html = button_click_to_new_tab(
+data_html_orange = button_click_to_new_tab(
     title='Orange',
     background_url='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a080db9e9849832eace72f_orange.jpg',
     href='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a080db9e9849832eace72f_orange.jpg',
 )
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=14, rows_size=1, cols_size=3,
-)
 
-
-html = button_click_to_new_tab(
+data_html_voice = button_click_to_new_tab(
     title='Voice',
     background_url='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a080db484a3c2ebeb21a0d_voice.jpg',
     href='https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a080db484a3c2ebeb21a0d_voice.jpg',
 )
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=15, rows_size=1, cols_size=3,
-)
 
+data_html_revenue_prediction = create_h1_title(title='Revenue prediction', subtitle='Revenue in €')
 
-html = create_h1_title(title='Revenue prediction', subtitle='Revenue in €')
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=16, rows_size=1, cols_size=12,
-)
-df = pd.read_csv('../../data/test/pred_data.csv')
-df['date'] = pd.to_datetime(df['date']).dt.date
-min_date: str = '2022-06-12'
-s.plt.predictive_line(
-    # title='Revenue prediction',
-    data=df.to_dict(orient='records'), x='date', y=['billing'],
-    min_value_mark=min_date,
-    max_value_mark=df['date'].max().isoformat(),
-    menu_path=menu_path,
-    order=17, rows_size=2, cols_size=12,
-)
-
-
-html = box_with_button(
+data_html_boxbutton = box_with_button(
     href='https://www.shimoku.com',
     title='Shimoku is a White Label App',
     line='Share with your clients your own App,  not an Standard',
 )
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=18, rows_size=2, cols_size=7,
+
+data_html_panel = panel(
+    href='www.shimoku.com',
+    text='Attention, this is a brief explanation about the information in this section',
 )
-
-
-html = panel(href='www.shimoku.com', text='Attention, this is a brief explanation about the information in this section')
-s.plt.html(
-    html=html,
-    menu_path=menu_path,
-    order=19, rows_size=1, cols_size=5,
-)
-
-s.plt.set_apps_orders({'overview': 1})
---
+++ templates/portfolio.py
@@ -0,0 +1,188 @@
+"""
+"""
+
+from os import getenv
+
+import datetime as dt
+import pandas as pd
+
+import shimoku_api_python as shimoku
+
+from data.portfolio_data import (
+    data_html_real_time, data_indicator_real_time_events,
+    data_indicator_real_time_time_session,
+    data_html_summary, data_line_sessions_date,
+    data_pie, data_html_beautiful_indicator_retention_suite,
+    data_html_beautiful_indicator_anomaly_suite,
+    data_html_title_cohort, data_heatmap,
+    data_html_creativity, data_html_pug,
+    data_html_orange, data_html_chupa_chups, data_html_voice,
+    data_html_boxbutton, data_html_revenue_prediction,
+    data_html_panel, data_html_beautiful_indicator_stock_suite,
+)
+
+api_key: str = getenv('API_TOKEN')
+universe_id: str = getenv('UNIVERSE_ID')
+business_id: str = getenv('BUSINESS_ID')
+environment: str = getenv('ENVIRONMENT')
+
+
+s = shimoku.Client(
+    config={'access_token': api_key},
+    universe_id=universe_id,
+    environment=environment,
+)
+s.plt.set_business(business_id=business_id)
+menu_path: str = 'Portfolio'
+create_rt_indicators: bool = True
+
+
+s.plt.html(
+    html=data_html_real_time,
+    menu_path=menu_path,
+    order=0, rows_size=1, cols_size=12,
+)
+
+
+if create_rt_indicators:
+    s.plt.indicator(
+        data=data_indicator_real_time_events,
+        menu_path=menu_path,
+        order=1, rows_size=1, cols_size=6,
+        value='value',
+        color='color',
+        header='title',
+        footer='description',
+        align='align'
+    )
+
+    s.plt.indicator(
+        data=data_indicator_real_time_time_session,
+        menu_path=menu_path,
+        order=2, rows_size=1, cols_size=6,
+        value='value',
+        color='color',
+        header='title',
+        footer='description',
+        align='align'
+    )
+
+
+s.plt.html(
+    html=data_html_summary,
+    menu_path=menu_path,
+    order=3, rows_size=1, cols_size=12,
+)
+
+
+s.plt.line(
+    data=data_line_sessions_date,
+    x='date', y=['Sessions'],
+    menu_path=menu_path,
+    order=4, rows_size=2, cols_size=7,
+    title='Total Sessions x day',
+    option_modifications={'dataZoom': False}
+)
+
+s.plt.pie(
+    data=data_pie,
+    x='name', y='value',
+    menu_path=menu_path,
+    order=5, rows_size=2, cols_size=5,
+    title='Devices',
+)
+
+
+s.plt.html(
+    html=data_html_beautiful_indicator_anomaly_suite,
+    menu_path=menu_path,
+    order=6, rows_size=1, cols_size=4,
+)
+
+s.plt.html(
+    html=data_html_beautiful_indicator_retention_suite,
+    menu_path=menu_path,
+    order=7, rows_size=1, cols_size=4,
+)
+
+s.plt.html(
+    html=data_html_beautiful_indicator_stock_suite,
+    menu_path=menu_path,
+    order=8, rows_size=1, cols_size=4,
+)
+
+s.plt.html(
+    html=data_html_title_cohort,
+    menu_path=menu_path,
+    order=9, rows_size=1, cols_size=12,
+)
+
+s.plt.heatmap(
+        data=data_heatmap,
+        x='xAxis', y='yAxis',
+        value='value',
+        menu_path=menu_path,
+        order=10, rows_size=2, cols_size=12,
+)
+
+s.plt.html(
+    html=data_html_creativity,
+    menu_path=menu_path,
+    order=11, rows_size=1, cols_size=12,
+)
+
+s.plt.html(
+    html=data_html_pug,
+    menu_path=menu_path,
+    order=12, rows_size=1, cols_size=3,
+)
+
+s.plt.html(
+    html=data_html_chupa_chups,
+    menu_path=menu_path,
+    order=13, rows_size=1, cols_size=3,
+)
+
+s.plt.html(
+    html=data_html_orange,
+    menu_path=menu_path,
+    order=14, rows_size=1, cols_size=3,
+)
+
+s.plt.html(
+    html=data_html_voice,
+    menu_path=menu_path,
+    order=15, rows_size=1, cols_size=3,
+)
+
+s.plt.html(
+    html=data_html_revenue_prediction,
+    menu_path=menu_path,
+    order=16, rows_size=1, cols_size=12,
+)
+
+df = pd.read_csv('../data/portfolio_predictive_line.csv')
+df['date'] = pd.to_datetime(df['date']).dt.date
+min_date: str = '2022-06-12'
+s.plt.predictive_line(
+    # title='Revenue prediction',
+    data=df.to_dict(orient='records'), x='date', y=['billing'],
+    min_value_mark=min_date,
+    max_value_mark=df['date'].max().isoformat(),
+    menu_path=menu_path,
+    order=17, rows_size=2, cols_size=12,
+)
+
+s.plt.html(
+    html=data_html_boxbutton,
+    menu_path=menu_path,
+    order=18, rows_size=2, cols_size=7,
+)
+
+s.plt.html(
+    html=data_html_panel,
+    menu_path=menu_path,
+    order=19, rows_size=1, cols_size=5,
+)
+
+s.plt.set_apps_orders({'overview': 1})
