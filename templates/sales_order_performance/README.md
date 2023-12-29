592d445d9183f1b64bd70169221292f7d2413da9
arturolinares24 <arturo.linares@shimoku.com>
2023-12-29 08:17:49 -0500

    [FP-07] Financial Sales Order Performance

---
+++ templates/sales_order_performance/README.md
@@ -0,0 +1,91 @@
+# Sales Order Performance Dashboard
+
+This dashboard showcases the capabilities of the Shimoku SDK using the context of Sales Order Performance. The dataset revolves around sales orders, expenses, and the visualization of monthly income and expenses, along with the corresponding profits, over time. The analysis provides a comprehensive view of the financial performance on a month-to-month basis.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Python 3.x installed on your system. You can download it from python.org.
+
+- pip (Python Package Installer), should come installed with Python.
+
+## Installation
+
+Follow these steps to set up the project locally.
+
+### Clone the repository:
+
+```
+git clone https://github.com/your_username/your_project.git
+```
+```
+cd your_project
+```
+
+Create a virtual environment:
+
+```
+python3 -m venv venv
+```
+  
+On Windows, you might need to use python instead of python3.
+
+Activate the virtual environment: 
+
+On Linux/Mac:
+
+```
+source venv/bin/activate
+```
+  
+On Windows:
+```
+ .\venv\Scripts\activate
+```
+
+Install the required packages:
+
+```
+pip install -r requirements.txt
+```
+  
+
+## Configuring Environment Variables
+
+The project requires certain environment variables to be set. These variables can be found in the .env.example file. To set them up:
+
+Create a new file in the project root directory named .env.
+
+Replace the empty values with your specific configurations:
+```
+API_TOKEN=""
+UNIVERSE_ID=""
+WORKSPACE_ID=""
+```
+
+## Generate example dataset
+
+To generate the example dataset, execute the following command:
+
+```
+python3 generate_sales_orders_performance.py
+```
+
+## Running the Application
+
+After completing the dataset generation and ensuring that the environment variables are correctly set, you can launch the application using the following command:
+
+```
+python3 app.py
+```
+
+## Screens
+
+<p align="center">
+  <img src="img/screen.JPG">
+</p>
---
+++ templates/sales_order_performance/board.py
@@ -0,0 +1,75 @@
+from shimoku_api_python import Client
+from utils.utils import get_data, process_sales_data
+
+
+class Board:
+    """
+    A class representing a Dashboard for displaying various data visualizations.
+
+    Attributes:
+        board_name (str): Name of the dashboard.
+        df (DF): An instance of a DF class for handling data frames.
+        shimoku (Client): An instance of a Client class for Shimoku API interactions.
+        results (dict): A dictionary to store processed data for use in plotting.
+    """
+
+    def __init__(self, shimoku: Client):
+        """
+        Constructor for the Dashboard class.
+
+        Parameters:
+            shimoku (Client): An instance of a Client class for Shimoku API interactions.
+        """
+
+        file_names = ["data/sales_orders_performance.csv"]
+        self.board_name = "FP-Sales Order Performance"  # Name of the dashboard
+        self.df = get_data(file_names)
+        self.shimoku = shimoku  # Shimoku client instance
+        self.shimoku.set_board(name=self.board_name)  # Setting up the board in Shimoku
+        self.results = None  # Placeholder for storing processed data
+
+    def transform(self):
+        """
+        Perform data transformations.
+
+        This method is responsible for handling any data transformations
+        required before plotting the data on the dashboard.
+        """
+
+        df = self.df["sales_orders_performance"]
+
+        # Process sales data
+        (
+            income_total,
+            spend_total,
+            net_profit,
+            average_profit_per_order,
+            net_profit_by_month,
+        ) = process_sales_data(df)
+
+        # Store processed data in results attribute
+        self.results = {
+            "income_total": income_total,
+            "spend_total": spend_total,
+            "net_profit": net_profit,
+            "average_profit_per_order": average_profit_per_order,
+            "net_profit_by_month": net_profit_by_month,
+        }
+
+        return True
+
+    def plot(self):
+        """
+        Plot the dashboard.
+
+        This method initializes and calls the plot method of a specific path
+        class responsible for plotting the Sales Order Performance dashboard.
+
+        Returns:
+        None. The function is used for its side effect of plotting data.
+        """
+
+        from paths.sales_order_perfomance import SalesOrderPerformance
+
+        sales_order_performance = SalesOrderPerformance(self)
+        sales_order_performance.plot()
