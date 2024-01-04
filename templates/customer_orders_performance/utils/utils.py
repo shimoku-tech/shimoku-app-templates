bb73fe681b0c9f73387deda77b8b06db85132849
jkahn <joseph.kahn@shimoku.com>
2024-01-04 13:27:44 -0500

    fix: add header on the dashboard

--- templates/customer_orders_performance/utils/utils.py
+++ templates/customer_orders_performance/utils/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from re import sub
 
 
 def get_data(file_names):
@@ -46,3 +47,34 @@ def convert_dataframe_to_array(df):
         new_data.append(new_dict)
 
     return new_data
+
+def beautiful_indicator(title):
+    # title_style: str = sub(r"\s+", "", title)
+    return (
+        "<head>"
+        "<style>"  # Styles title
+        ".component-title{height:auto; width:100%; "
+        "border-radius:16px; padding:16px;"
+        "display:flex; align-items:center;"
+        "background-color:var(--chart-C1); color:var(--color-white);}"
+        "</style>"
+        # Start icons style
+        "<style>.big-icon-banner"
+        "{width:48px; height: 48px; display: flex;"
+        "margin-right: 16px;"
+        "justify-content: center;"
+        "align-items: center;"
+        "background-size: contain;"
+        "background-position: center;"
+        "background-repeat: no-repeat;"
+        "background-image: url('https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/63594ccf3f311a98d72faff7_suite-customer-b.svg');}"
+        "</style>"
+        # End icons style
+        "<style>.base-white{color:var(--color-white);}</style>"
+        "</head>"  # Styles subtitle
+        "<div class='component-title'>"
+        "<div class='big-icon-banner'></div>"
+        "<div class='text-block'>"
+        "<h1>" + title + "</h1>"
+        "</div>"
+    )
