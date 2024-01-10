25d6296ac6708304d0f67c5b8d891d320819d24e
Felipe Idme <felipeidme04@gmail.com>
2024-01-10 09:14:21 -0500

    Code formatted with black

--- templates/ecommerce_facebook_ads/board.py
+++ templates/ecommerce_facebook_ads/board.py
@@ -4,6 +4,7 @@ import pandas as pd
 import calendar
 import numpy as np
 
+
 class Board:
     """
     A class used to represent a Dashboard for displaying various data visualizations.
@@ -19,11 +20,11 @@ class Board:
 
         file_names = ["data/facebook_ads.csv"]
         # Name of the dashboard
-        self.board_name = "Facebook Ads"  
+        self.board_name = "Facebook Ads"
         self.dfs = get_data(file_names)
 
         # Shimoku client instance
-        self.shimoku = shimoku  
+        self.shimoku = shimoku
         self.shimoku.set_board(name=self.board_name)
         self.shimoku.boards.update_board(name=self.board_name, is_public=True)
 
@@ -40,31 +41,37 @@ class Board:
         # To replace numbers for month names
         month_names = list(calendar.month_abbr)[1:]
         month_dict = {i: abbr_name for i, abbr_name in enumerate(month_names, start=1)}
-        
-        #Indicators data
-        ad_spend = round(df["ad_cost"].sum(),2)
-        cpm = round(df["ad_cost"].mean(),2)*1000
-        cpc = round((df["ad_cost"].sum())/(df["click"].sum()),2)
-        ctr = round((len(df))/(df["click"].sum()),2)
 
-        #Stacked bar data
+        # Indicators data
+        ad_spend = round(df["ad_cost"].sum(), 2)
+        cpm = round(df["ad_cost"].mean(), 2) * 1000
+        cpc = round((df["ad_cost"].sum()) / (df["click"].sum()), 2)
+        ctr = round((len(df)) / (df["click"].sum()), 2)
+
+        # Stacked bar data
         df["month"] = df["impression_date"].dt.month
-        ad_name_by_month = df.groupby(['month', 'ad_name']).size().reset_index(name='count')
-        ad_name_by_month = ad_name_by_month.pivot(index='month', columns='ad_name', values='count').fillna(0).astype(int).reset_index()
+        ad_name_by_month = (
+            df.groupby(["month", "ad_name"]).size().reset_index(name="count")
+        )
+        ad_name_by_month = (
+            ad_name_by_month.pivot(index="month", columns="ad_name", values="count")
+            .fillna(0)
+            .astype(int)
+            .reset_index()
+        )
         ad_name_by_month["month"] = ad_name_by_month["month"].replace(month_dict)
 
-        #Line data
-        ad_clicks = groupby_sum(df,df["impression_date"].dt.month,"click")
+        # Line data
+        ad_clicks = groupby_sum(df, df["impression_date"].dt.month, "click")
         ad_clicks["impression_date"] = ad_clicks["impression_date"].replace(month_dict)
 
-
         self.df_app = {
-            "ad_spend":ad_spend,
-            "cpm":cpm,
-            "cpc":cpc,
-            "ctr":ctr,
-            "ad_name_by_month":ad_name_by_month,
-            "ad_clicks":ad_clicks
+            "ad_spend": ad_spend,
+            "cpm": cpm,
+            "cpc": cpc,
+            "ctr": ctr,
+            "ad_name_by_month": ad_name_by_month,
+            "ad_clicks": ad_clicks,
         }
 
         return True
