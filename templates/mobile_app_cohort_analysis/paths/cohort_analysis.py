45fc4147097f944c8d0a1ef19bac6702bb61470d
jkahn <joseph.kahn@shimoku.com>
2024-01-16 16:12:07 -0500

    feat: Create three function to reuse the plot in each tab. Add docstring

--- templates/mobile_app_cohort_analysis/paths/cohort_analysis.py
+++ templates/mobile_app_cohort_analysis/paths/cohort_analysis.py
@@ -36,13 +36,13 @@ class CohortAnalysis(Board):
         """
         self.plot_header()
         self.plot_kpi_indicators()
-        self.set_tabs()
+        self.plot_tabs()
         self.plot_all()
         self.plot_gender()
         self.plot_age()
         self.plot_acquisition_source()
 
-    def plot_header(self):
+    def plot_header(self) -> bool:
         """Header plot of the menu path
 
         Returns:
@@ -81,7 +81,12 @@ class CohortAnalysis(Board):
 
         return True
 
-    def set_tabs(self):
+    def plot_tabs(self) -> bool:
+        """plot the principal tabs on the dashboard
+
+        Returns:
+            bool: Execution status
+        """
         self.shimoku.plt.set_tabs_index(("Charts", "All"),
             order=self.order,
             just_labels=True, cols_size = 22,
@@ -96,91 +101,45 @@ class CohortAnalysis(Board):
         return True
 
     def plot_all(self) -> bool:
+        """Plot the principal tab called 'All'. It plots two sections: Users Life Time and Cohort Analysis.
+
+        Returns:
+            bool: Execution status
+        """
+        # Change current tab
         self.shimoku.plt.change_current_tab("All")
 
-        # Lie chart
-        self.shimoku.plt.line(
-            data = self.df_app["all_life_time"],
-            order = self.order,
-            cols_size = 10,
-            rows_size = 3,
-            title = "Users Life Time",
-            x = "week",
-            x_axis_name="Weeks",
-        )
-        self.order += 1
+        # User life time - line chart
+        self.plot_users_life_time("all_life_time", 10)
 
-        # Table Chart
-        self.shimoku.plt.table(
-            data=self.df_app["all_cohort"],
-            order=self.order,
-            cols_size = 10,
-            rows_size = 4,
-            title="Cohort Analysis",
-            initial_sort_column="Week (Date)",
-            sort_descending=False,
-            label_columns={
-                f"W{index}": cohort_colors()
-            for index in range(self.df_app["all_cohort"].columns.shape[0] - 2)},
-            columns_options={
-                "Week (Date)": {"width": 110},
-                "Users": {"width": 80},
-            },
-        )
-        self.order += 1
+        # Cohort analysis by gender - table chart
+        self.plot_cohort_analysis(f"all_cohort")
 
         return True
 
     def plot_gender(self) -> bool:
+        """Plot the principal tab called "Gender". It plots three sections: Category,
+        Users Life Time and Cohort Analysis.
+
+        Returns:
+            bool: Execution status
+        """
+        # Change current tab
         self.shimoku.plt.change_current_tab("Gender")
 
         # Set bentobox size
         self.shimoku.plt.set_bentobox(cols_size=4, rows_size=3)
-
-        # pie chart
-        self.shimoku.plt.pie(
-            data = self.df_app["gender_category"],
-            order = self.order,
-            cols_size = 24,
-            rows_size = 21,
-            names = "name",
-            values = "value",
-            padding="2,2,2,2",
-        )
-        self.order += 1
-
-        # Html
-        self.shimoku.plt.html(
-            order = self.order,
-            cols_size = 24,
-            rows_size = 10,
-            padding="0,2,1,2",
-            html = categories(self.df_app["gender_category"].sort_values(
-                by=["value"],
-                ascending=False,
-            )),
-        )
-        self.order += 1
-
+        # Gender category - pie chart
+        self.plot_category("gender_category")
         # Pop out of bentobox
         self.shimoku.plt.pop_out_of_bentobox()
 
-        # Lie chart
-        self.shimoku.plt.line(
-            data = self.df_app["gender_life_time"],
-            order = self.order,
-            cols_size = 6,
-            rows_size = 3,
-            title = "Users Life Time",
-            x = "week",
-            x_axis_name="Weeks",
-        )
-        self.order += 1
+        # User life time - line chart
+        self.plot_users_life_time("gender_life_time")
 
         # Set bentobox size
         self.shimoku.plt.set_bentobox(cols_size=12, rows_size=5)
-
-        # Table Tabs
+        # Plot cohort analysis table in a diferent tab
         for gender in self.df_app["gender_category"].name.unique():
             self.shimoku.plt.set_tabs_index(
                 ("Gender Tabs", gender),
@@ -188,87 +147,44 @@ class CohortAnalysis(Board):
                 just_labels=True,
                 cols_size = 24,
                 rows_size = 6,
-                padding='0,0,0,0',
                 parent_tabs_index=("Charts", "Gender"),
             )
             self.order += 1
 
-            # Table Chart
-            self.shimoku.plt.table(
-                data=self.df_app[f"gender_cohort_{gender}"],
-                order=self.order,
-                cols_size = 24,
-                rows_size = 4,
-                title="Cohort Analysis",
-                initial_sort_column="Week (Date)",
-                sort_descending=False,
-                label_columns={
-                    f"W{index}": cohort_colors()
-                for index in range(self.df_app[f"gender_cohort_{gender}"].columns.shape[0] - 2)},
-                columns_options={
-                    "Week (Date)": {"width": 110},
-                    "Users": {"width": 80},
-                },
-            )
-            self.order += 1
-
-        self.shimoku.plt.set_tabs_index(tabs_index=("Charts", "Gender"))
+            # Cohort analysis by gender - table chart
+            self.plot_cohort_analysis(f"gender_cohort_{gender}")
 
         # Pop out of bentobox
         self.shimoku.plt.pop_out_of_bentobox()
+
+        # Go back to principal tabs
+        self.shimoku.plt.set_tabs_index(tabs_index=("Charts", "Gender"))
 
         return True
 
     def plot_age(self) -> bool:
+        """Plot the principal tab called "Age". It plots three sections: Category,
+        Users Life Time and Cohort Analysis.
+
+        Returns:
+            bool: Execution status
+        """
+        # Change current tab
         self.shimoku.plt.change_current_tab("Age")
 
         # Set bentobox size
         self.shimoku.plt.set_bentobox(cols_size=4, rows_size=3)
-
-        # pie chart
-        self.shimoku.plt.pie(
-            data = self.df_app["age_category"],
-            order = self.order,
-            cols_size = 24,
-            rows_size = 18,
-            names = "name",
-            values = "value",
-            padding="2,2,2,2",
-        )
-        self.order += 1
-
-        # Html
-        self.shimoku.plt.html(
-            order = self.order,
-            cols_size = 24,
-            rows_size = 10,
-            padding="0,2,1,2",
-            html = categories(self.df_app["age_category"].sort_values(
-                by=["value"],
-                ascending=False,
-            )),
-        )
-        self.order += 1
-
+        # Age category - Pie Chart
+        self.plot_category("age_category")
         # Pop out of bentobox
         self.shimoku.plt.pop_out_of_bentobox()
 
-        # Lie chart
-        self.shimoku.plt.line(
-            data = self.df_app["age_life_time"],
-            order = self.order,
-            cols_size = 6,
-            rows_size = 3,
-            title = "Users Life Time",
-            x = "week",
-            x_axis_name="Weeks",
-        )
-        self.order += 1
+        # User life time - line chart
+        self.plot_users_life_time("age_life_time")
 
         # Set bentobox size
         self.shimoku.plt.set_bentobox(cols_size=12, rows_size=5)
-
-        # Table Tabs
+        # Plot cohort analysis table in a diferent tab
         for age_range in self.df_app["age_category"].name.unique():
             self.shimoku.plt.set_tabs_index(
                 ("Age Tabs", age_range),
@@ -278,82 +194,40 @@ class CohortAnalysis(Board):
             )
             self.order += 1
 
-            self.shimoku.plt.table(
-                data=self.df_app[f"age_cohort_{age_range}"],
-                order=self.order,
-                cols_size = 24,
-                rows_size = 4,
-                title="Cohort Analysis",
-                initial_sort_column="Week (Date)",
-                sort_descending=False,
-                label_columns={
-                    f"W{index}": cohort_colors()
-                for index in range(self.df_app[f"age_cohort_{age_range}"].columns.shape[0] - 2)},
-                columns_options={
-                    "Week (Date)": {"width": 110},
-                    "Users": {"width": 80},
-                },
-            )
-            self.order += 1
-            self.order += 1
+            # Cohort analysis by age range - table chart
+            self.plot_cohort_analysis(f"age_cohort_{age_range}")
 
         # Pop out of bentobox
         self.shimoku.plt.pop_out_of_bentobox()
+
+        # Go back to principal tabs
         self.shimoku.plt.set_tabs_index(tabs_index=("Charts", "Age"))
 
         return True
 
     def plot_acquisition_source(self) -> bool:
+        """Plot the principal tab called "Acquisition Source". It plots three sections: Category,
+        Users Life Time and Cohort Analysis.
+
+        Returns:
+            bool: Execution status
+        """
+        # Change Current Tab
         self.shimoku.plt.change_current_tab("Acquisition Source")
 
         # Set bentobox size
         self.shimoku.plt.set_bentobox(cols_size=4, rows_size=3)
-
-        # Pie chart
-        self.shimoku.plt.pie(
-            data = self.df_app["source_category"],
-            order = self.order,
-            cols_size = 24,
-            rows_size = 21,
-            names = "name",
-            values = "value",
-            padding="2,2,2,2",
-        )
-        self.order += 1
-
-        # Html
-        self.shimoku.plt.html(
-            order = self.order,
-            cols_size = 24,
-            rows_size = 10,
-            padding="0,2,1,2",
-            html = categories(self.df_app["source_category"].sort_values(
-                by=["value"],
-                ascending=False,
-            ))
-        )
-        self.order += 1
-
+        # Gender category - pie chart
+        self.plot_category("source_category")
         # Pop out of bentobox
         self.shimoku.plt.pop_out_of_bentobox()
 
-        # Line chart
-        self.shimoku.plt.line(
-            data = self.df_app["source_life_time"],
-            order = self.order,
-            cols_size = 6,
-            rows_size = 3,
-            title = "Users Life Time",
-            x = "week",
-            x_axis_name="Weeks",
-        )
-        self.order += 1
-
+        # User life time - line chart
+        self.plot_users_life_time("source_life_time")
 
         # Set bentobox size
         self.shimoku.plt.set_bentobox(cols_size=12, rows_size=6)
-
-        # Table Tabs
+        # Plot cohort analysis table in a diferent tab
         for source in self.df_app["source_category"].name.unique():
             self.shimoku.plt.set_tabs_index(
                 ("Acquisition Source Tabs", source),
@@ -365,28 +239,106 @@ class CohortAnalysis(Board):
             )
             self.order += 1
 
-            # Table Chart
-            self.shimoku.plt.table(
-                data=self.df_app[f"source_cohort_{source}"],
-                order=self.order,
-                cols_size = 24,
-                rows_size = 4,
-                title="Cohort Analysis",
-                initial_sort_column="Week (Date)",
-                sort_descending=False,
-                label_columns={
-                    f"W{index}": cohort_colors()
-                for index in range(self.df_app[f"source_cohort_{source}"].columns.shape[0] - 2)},
-                columns_options={
-                    "Week (Date)": {"width": 110},
-                    "Users": {"width": 80},
-                },
-            )
-            self.order += 1
+            # Cohort analysis by adquisition source - table chart
+            self.plot_cohort_analysis(f"source_cohort_{source}")
 
         # Pop out of bentobox
         self.shimoku.plt.pop_out_of_bentobox()
 
+        # Go back to principal tabs
         self.shimoku.plt.set_tabs_index(tabs_index=("Charts", "Acquisition Source"))
+
+        # Pop out of tabs group
         self.shimoku.plt.pop_out_of_tabs_group()
+
+        return True
+
+    def plot_category(self, dataframe_name: str, ):
+        """Function basic to plot the category section for each tab
+
+        Args:
+            dataframe_name (str): Dataframe name used  in the dictionary of dataframes.
+
+        Returns:
+            bool: Execution status
+        """
+        # Pie chart
+        self.shimoku.plt.pie(
+            data = self.df_app[dataframe_name],
+            order = self.order,
+            cols_size = 24,
+            rows_size = 21,
+            names = "name",
+            values = "value",
+            padding="2,2,2,2",
+        )
+        self.order += 1
+
+        # Html
+        self.shimoku.plt.html(
+            order = self.order,
+            cols_size = 24,
+            rows_size = 10,
+            padding="0,2,1,2",
+            html = categories(self.df_app[dataframe_name].sort_values(
+                by=["value"],
+                ascending=False,
+            ))
+        )
+        self.order += 1
+
+        return True
+
+    def plot_users_life_time(self, dataframe_name: str, cols_size: int = 6):
+        """Function basic to plot the User Life Time section for each tab
+
+        Args:
+            dataframe_name (str): Dataframe name used  in the dictionary of dataframes.
+            cols_size (int, optional): Columns size to plot the chart. Defaults to 6.
+
+        Returns:
+            bool: Execution status
+        """
+        # Line chart
+        self.shimoku.plt.line(
+            data = self.df_app[dataframe_name],
+            order = self.order,
+            cols_size = cols_size,
+            rows_size = 3,
+            title = "Users Life Time",
+            x = "week",
+            x_axis_name="Weeks",
+        )
+        self.order += 1
+
+        return True
+
+    def plot_cohort_analysis(self, dataframe_name: str):
+        """Function basic to plot the Cohort Analysis section for each tab
+
+        Args:
+            dataframe_name (str): Dataframe name used  in the dictionary of dataframes.
+
+        Returns:
+            bool: Execution status
+        """
+        # Table Chart
+        self.shimoku.plt.table(
+            data=self.df_app[dataframe_name],
+            order=self.order,
+            cols_size = 24,
+            rows_size = 4,
+            title="Cohort Analysis",
+            initial_sort_column="Week (Date)",
+            sort_descending=False,
+            label_columns={
+                f"W{index}": cohort_colors()
+            for index in range(self.df_app[dataframe_name].columns.shape[0] - 2)},
+            columns_options={
+                "Week (Date)": {"width": 110},
+                "Users": {"width": 80},
+            },
+        )
+        self.order += 1
+
         return True
