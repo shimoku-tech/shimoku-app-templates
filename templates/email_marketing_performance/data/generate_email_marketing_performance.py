bf38bf35d552efe9b5d46b684e44d74d788fae90
jkahn <joseph.kahn@shimoku.com>
2024-02-13 14:39:33 -0500

    feat: data fake

--- templates/email_marketing_performance/data/generate_email_marketing_performance.py
+++ templates/email_marketing_performance/data/generate_email_marketing_performance.py
@@ -63,11 +63,6 @@ def generate_data() -> pd.DataFrame:
         days = [day.strftime('%m/%d/%Y') for day in days]
         return " ".join(days)
 
-    def chunks(L, n):
-        """ Yield successive n-sized chunks from L.
-        """
-        return [L[i:i+n] for i in range(0, len(L), n)]
-
     def chunk_into_n(lst: list, n: int) -> list:
         """Return a list with the number of elements of the n subgroup generate of lst
 
@@ -98,13 +93,30 @@ def generate_data() -> pd.DataFrame:
             return True if flag else False
 
 
+    def random_sum_to(n: int, num_terms: int = None) -> list:
+        """Return random num_terms-size sequence of numbers that sumatory is equal to n
+
+        Args:
+            n (int): Integer number.
+            num_terms (int, optional): Size of the sequence. Defaults to None.
+
+        Returns:
+            list: Sequence of numbers.
+        """
+
+        num_terms = (num_terms or random.randint(2, n)) - 1
+        a = random.sample(range(1, n), num_terms) + [0, n]
+        list.sort(a)
+        return [a[i+1] - a[i] for i in range(len(a) - 1)]
+
+
     # Campaign CSV
     campaign_ids = [
         "".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", k=8))
     for _ in range(total_campaign)]
 
     campaign_names = [
-        " ".join(["Campaña", "".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", k=2))])
+        " ".join(["Campaign", "".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", k=2))])
     for _ in range(total_campaign)]
 
     campaign_start_date = [
@@ -122,10 +134,10 @@ def generate_data() -> pd.DataFrame:
     for _ in range(total_models)]
 
     model_names = [
-        "".join(["Model", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[i]])
+        " ".join(["Model", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[i]])
     for i in range(total_models)]
 
-    # ToDo: generar texto con lipsum
+    # ToDo: generar texto con lorem ipsum
     subjects = [
         "Shimoku, la Inteligencia Artificial (IA) en la que se apoyan empresas como Amazon",
         "Shimoku, tu proyecto llave en mano de Inteligencia Artificial (IA) para sacarle el máximo partido a tu software",
@@ -149,26 +161,24 @@ def generate_data() -> pd.DataFrame:
     # Model by Campaign CSV
     model_by_campaign_ids = [
         "".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", k=8))
-    for _ in range(total_model_by_campaign * total_campaign)]
+    for _ in range(total_models)]
 
-    foreign_key_campaign_ids = [
-        campaign_ids[ i // 2]
-    for i in range(total_model_by_campaign * total_campaign)]
-
-    chunk_model_ids = chunks(model_ids, total_model_by_campaign)
-
-    foreign_key_model_ids = [
-        model
-    for i in range(total_campaign) for model in chunk_model_ids[i]]
-
-    # delivery_days  = [
-    #     random_secuence(date_ini, date_end)
-    # for _ in range(total_campaign)]
+    numbers_model_by_campaign = random_sum_to(total_models, total_campaign)
+    foreign_key_campaign_ids = []
+    foreign_key_model_ids = []
+    delivery_days  = []
+    index_campaign = 0
+    for index_model in range(total_models):
+        if index_model > sum(numbers_model_by_campaign[:index_campaign + 1]):
+            index_campaign += 1
+        foreign_key_campaign_ids.append(campaign_ids[index_campaign])
+        foreign_key_model_ids.append(model_ids[index_model])
+        delivery_days.append(random_secuence(date_ini, date_end))
 
 
     # Email CSV
     counts = [len(group) for group in chunk_into_n(client_id, total_models)]
-    model_by_client = random.sample(model_by_campaign_ids, counts=counts, k=total_clients)
+    model_by_client = random.sample(model_ids, counts=counts, k=total_clients)
 
     rebound_flag = (
         [True] * int(total_clients * 0.2)
@@ -187,41 +197,42 @@ def generate_data() -> pd.DataFrame:
 
     email_client_key = []
     email_rebound = []
-    email_delivery_days = []
+    email_model_by_campaign = []
+    email_delivery_date = []
 
-    # number_emails = lambda flag: 4 if not flag else 1
-    # for client_index in range(total_clients):
-    #     rebound_email = rebound_flag[client_index]
-    #     n_emails = number_emails(rebound_email)
-    #     for _ in range(n_emails):
-    #         email_client_key += [client_id[client_index]]
-    #         email_rebound += [rebound_email]
+    number_emails = lambda flag: 4 if not flag else 1
+    for index_client in range(total_clients):
+        rebound_email = rebound_flag[index_client]
+        n_emails = number_emails(rebound_email)
+        for index_email in range(n_emails):
+            email_client_key += [client_id[index_client]]
+            email_rebound += [rebound_email]
+            email_index_model = model_ids.index(model_by_client[index_client])
+            email_model_by_campaign += [model_by_campaign_ids[email_index_model]]
+            email_date = delivery_days[email_index_model].split(" ")[index_email]
+            email_delivery_date.append(email_date)
 
+    open_flag = [
+        random_bolean(
+            email_rebound[i],
+            random.random() < 0.4
+        )
+        for i in range(total_emails)
+    ]
 
+    click_number = [
+        random.randint(0,3) if (not email_rebound[i] and open_flag[i]) else None
+        for i in range(total_emails)
+    ]
 
-    # open_flag = [
-    #     random_bolean(
-    #         email_rebound[i],
-    #         random.random() < 0.4
-    #     )
-    #     for i in range(total_emails)
-    # ]
+    answer_flag = [
+        random_bolean(
+            email_rebound[i],
+            (random.random() < 0.01) & (open_flag[i] if pd.notna(open_flag[i]) else False)
+        )
+        for i in range(total_emails)
+    ]
 
-    # click_flag = [
-    #     random_bolean(
-    #         email_rebound[i],
-    #         (random.random() < 0.06) & (open_flag[i] if pd.notna(open_flag[i]) else False)
-    #     )
-    #     for i in range(total_emails)
-    # ]
-
-    # answer_flag = [
-    #     random_bolean(
-    #         email_rebound[i],
-    #         (random.random() < 0.01) & (open_flag[i] if pd.notna(open_flag[i]) else False)
-    #     )
-    #     for i in range(total_emails)
-    # ]
 
     # Create the DataFrame
     data_campaign = {
@@ -230,37 +241,43 @@ def generate_data() -> pd.DataFrame:
         "start_date": campaign_start_date,
         "end_date": campaign_end_date,
     }
+
     data_model = {
         "id": model_ids,
         "name": model_names,
         "subject": subjects,
     }
+
     data_client = {
         "id": client_id,
         "first_name": first_names,
         "last_name": last_names,
         "company_name": company_names,
     }
+
     data_model_by_campaign = {
         "id" : model_by_campaign_ids,
         "id_campaign": foreign_key_campaign_ids,
         "id_model": foreign_key_model_ids,
-        # "delivery_days": delivery_days,
+        "delivery_days": delivery_days,
     }
+
     data_email = {
-        "email_id": email_ids,
-        # "rebound_flag": email_rebound,
-        # "open_flag": open_flag,
-        # "click_number": click_number,
-        # "answer_flag": answer_flag,
-        # "client_id": email_client_key,
+        "id": email_ids,
+        "id_client": email_client_key,
+        "id_campaign_model": email_model_by_campaign,
+        "rebound_flag": email_rebound,
+        "delivery_date": email_delivery_date,
+        "open_flag": open_flag,
+        "click_number": click_number,
+        "answer_flag": answer_flag,
     }
 
     df_compaign = pd.DataFrame(data_campaign)
     df_model = pd.DataFrame(data_model)
     df_client = pd.DataFrame(data_client)
     df_model_by_campaign = pd.DataFrame(data_model_by_campaign)
-    # df_email = pd.DataFrame(data_email)
+    df_email = pd.DataFrame(data_email)
 
     # Display the DataFrame
     print("\n### Total Models registers generated: " + str(len(df_compaign)))
@@ -275,24 +292,24 @@ def generate_data() -> pd.DataFrame:
     print("\n### Total Models by Campaign registers generated: " + str(len(df_model_by_campaign)))
     print("\n")
     print(df_model_by_campaign.head(10))
-    # print("\n### Total Emails registers generated: " + str(len(df_email)))
-    # print("\n")
-    # print(df_email.head(10))
-    # print("\n\n")
+    print("\n### Total Emails registers generated: " + str(len(df_email)))
+    print("\n")
+    print(df_email.head(10))
+    print("\n\n")
 
     # Save the dataframe to CSV
     df_compaign.to_csv(campaign_output_file, index=False)
     df_model.to_csv(model_output_file, index=False)
     df_client.to_csv(client_output_file, index=False)
     df_model_by_campaign.to_csv(model_by_campaign_output_file, index=False)
-    # df_email.to_csv(email_output_file, index=False)
+    df_email.to_csv(email_output_file, index=False)
 
     return {
         "compaign": df_compaign,
         "model": df_model,
         "client": df_client,
         "campaign_model": df_model_by_campaign,
-        # "email": df_email,
+        "email": df_email,
     }
 
 
